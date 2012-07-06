8e71a0f32168c59c20b9ce7aa2c49fccb3559ffc
ares1060 <scrapy_ii@gmx.at>
2012-07-06 14:34:09 +0200

    interface update

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,9 +1,13 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -23,7 +27,7 @@ public class GUI extends JFrame {
 	 * Instantiate user interface variables
 	 */
 	JFrame frame = new JFrame("Titeleditor");
-	JPanel panel1,panel2,panel3,panel4;
+	JPanel panel_video_big, panel_effects, panel_video, panel_settings, setting_timeline;
 	
 	
 
@@ -34,26 +38,34 @@ public class GUI extends JFrame {
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		panel1 = new JPanel(new GridBagLayout());
-		panel1.setBorder(BorderFactory.createTitledBorder("Video Effects"));
-		panel2 = new JPanel(new GridBagLayout());
-		panel2.setBorder(BorderFactory.createTitledBorder("Video Preview"));
-		panel3 = new JPanel(new GridBagLayout());
-		panel3.setBorder(BorderFactory.createTitledBorder("Text Styles"));
-		panel4 = new JPanel(new GridBagLayout());
-		panel4.setBorder(BorderFactory.createTitledBorder("Timeline"));
+		panel_effects = new JPanel(new GridBagLayout());
+		panel_effects.setBackground(new Color(255, 0, 0));
+		panel_effects.setBorder(BorderFactory.createTitledBorder("Video Effects"));
 		
+		panel_video = new JPanel(new GridBagLayout());
+		panel_video.setBorder(BorderFactory.createTitledBorder("Video Preview"));
 		
+		panel_settings = new JPanel(new GridBagLayout());
+		panel_settings.setBorder(BorderFactory.createTitledBorder("Text Styles"));
+		panel_settings.setSize(400, 400);
 		
+		setting_timeline = new JPanel(new GridBagLayout());
+		setting_timeline.setBorder(BorderFactory.createTitledBorder("Timeline"));
 		
-		frame.add(panel1,BorderLayout.PAGE_START);
-		frame.add(panel2, BorderLayout.CENTER);
-		frame.add(panel3,BorderLayout.EAST);
-		frame.add(panel4, BorderLayout.PAGE_END);
+		panel_video_big = new JPanel();
+		panel_video_big.setLayout(new BoxLayout(panel_video_big, BoxLayout.PAGE_AXIS));
+		panel_video_big.add(panel_effects);
+		panel_video_big.add(panel_video);
 		
-		frame.pack();
+		frame.add(panel_video_big,BorderLayout.CENTER);
+		frame.add(panel_settings,BorderLayout.EAST);
+		frame.add(setting_timeline, BorderLayout.PAGE_END);
+		
+		frame.setSize(800, 600);
+		
+		//frame.pack();
 		frame.setVisible(true);
-		frame.setResizable(true);
+		frame.setResizable(false);
 		
 	}
 }
